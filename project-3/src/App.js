2818ea558a6293a02da00b8d53074285e986d06c
thomas <tom_tyte@hotmail.com>
2024-11-26 11:15:51 -0500

    added navbar

--- project-3/src/App.js
+++ project-3/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Features/Navbar.jsx';
+
 
 function App() {
   return (
---
+++ project-3/src/Features/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/todos">Todos</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
